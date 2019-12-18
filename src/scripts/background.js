be75bd28278de47800fae2eed74ad89f96069bec
Gapen <greendev@163.com>
2019-12-18 14:52:09 +0800

    文件名

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -12,8 +12,8 @@ ext.runtime.onMessage.addListener(
   }
 );
 
-function down(url) {
-  chrome.downloads.download({url: url},
+function down(url, name) {
+  chrome.downloads.download({url: url, filename: name + '.crx'},
     function(id) {
       console.log(id);
     });
@@ -21,11 +21,15 @@ function down(url) {
 
 function downloader(params) {
   const url = params.pageUrl
-  const reg = url.match(/https:\/\/chrome\.google\.com\/webstore\/detail\/[^\/]+\/([^\?]+)\?/)
-  if (reg && reg.length === 2) {
-    const id = reg[1]
+  const reg = url.match(/https:\/\/chrome\.google\.com\/webstore\/detail\/([^\/]+)\/([^\?]+).*/)
+  if (reg && reg.length === 3) {
+    const name = reg[1].replace(/-/g, '_')
+    const id = reg[2]
+    var chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+    var downurl = `https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${chromeVersion}&acceptformat=crx3&x=id%3D${id}%26installsource%3Dondemand%26uc`
+    //var downurl = `https://chrome.google.com/webstore/download/${id}/package/main/crx/3`
     // window.open(`https://chrome.google.com/webstore/download/${id}/package/main/crx/3`)
-    down(`https://chrome.google.com/webstore/download/${id}/package/main/crx/3`)
+    down(downurl, name)
   }
 }
 
